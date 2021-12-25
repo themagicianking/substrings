f6f4c6c5e3bf2e584ada7a092af6340b5c9713e6
Thane Chaves <tmoonder@gmail.com>
2021-12-24 23:32:42 -0500

    The method now returns all of the substrings. The next step will be implementing the currect number of times each are present.

--- substrings.rb
+++ substrings.rb
@@ -1,12 +1,16 @@
 def substrings(string, dictionary)
-  x = dictionary.length
+  x = dictionary.length - 1
   matching_strings = {}
-  appearance = 0
     for i in 0..x
       current_word = dictionary[i].to_s
       if string.include? current_word
-        appearance = appearance + 1
-        puts appearance
+        if matching_strings[current_word]== nil
+          times_present = 1
+        else
+          times_present = matching_strings[current_word] + 1
+        end
+        matching_strings[current_word] = times_present 
       end
     end
+  puts matching_strings
 end
