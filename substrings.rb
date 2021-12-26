5ec50bf8e57ed86adc6de4f98d135d135fd811bc
Thane Chaves <tmoonder@gmail.com>
2021-12-26 16:47:54 -0500

    Finished adding in the ability to count instances of a substring.

--- substrings.rb
+++ substrings.rb
@@ -1,14 +1,11 @@
 def substrings(string, dictionary)
   x = dictionary.length - 1
+  y = string.length - 1
   matching_strings = {}
     for i in 0..x
       current_word = dictionary[i].to_s
-      if string.include? current_word
-        if matching_strings[current_word]== nil
-          times_present = 1
-        else
-          times_present = matching_strings[current_word] + 1
-        end
+      if string.downcase.include? current_word.downcase
+        times_present = string.downcase.scan(current_word.downcase).count
         matching_strings[current_word] = times_present 
       end
     end
